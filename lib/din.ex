f2c5afc2870ca7626a9466b0b10c9fef9947bf5c
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-11 18:22:33 -0600

    Initial commit

---
+++ lib/din.ex
@@ -0,0 +1,18 @@
+defmodule Din do
+  @moduledoc """
+  Documentation for Din.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Din.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/din_test.exs
@@ -0,0 +1,8 @@
+defmodule DinTest do
+  use ExUnit.Case
+  doctest Din
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
