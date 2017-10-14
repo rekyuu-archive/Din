5a089c246a521e7045a73a4db344152b97fe6d5a
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-13 23:40:48 -0600

    stopped running at runtime

--- mix.exs
+++ mix.exs
@@ -18,8 +18,7 @@ defmodule Din.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :httpoison],
-     mod: {Din.Example.Supervisor, []}]
+    [extra_applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
