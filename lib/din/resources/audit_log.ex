f95790584264cdfc83fe348152babc2ce1346ce0
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-13 18:46:06 -0600

    testing funcs

--- lib/din/resources/audit_log.ex
+++ lib/din/resources/audit_log.ex
@@ -1,49 +1,13 @@
 defmodule Din.Resources.AuditLog do
   alias Din.Error
   @moduledoc """
+  Resources for Audit Logs.
+
   Whenever an admin action is performed on the API, an entry is added to the respective guild's audit log. You can specify the reason by attaching the `X-Audit-Log-Reason` request header. This header supports url encoded utf8 characters.
   """
 
-  @typedoc "list of valid audit log events"
-  @type event :: :guild_update | :channel_create | :channel_update | :channel_delete | :channel_overwrite_create | :channel_overwrite_update | :channel_overwrite_delete | :member_kick | :member_prune | :member_ban_add | :member_ban_remove | :member_update | :member_role_update | :role_create | :role_update | :role_delete | :invite_create | :invite_update | :invite_delete | :webhook_create | :webhook_update | :webhook_delete | :emoji_create | :emoji_update | :emoji_delete | :message_delete
-
   @doc """
-  Used to give readable names to event opcodes.
-  """
-  @spec event_codes :: map
-  def event_codes do
-    %{
-      guild_update:              1,
-      channel_create:           10,
-      channel_update:           11,
-      channel_delete:           12,
-      channel_overwrite_create: 13,
-      channel_overwrite_update: 14,
-      channel_overwrite_delete: 15,
-      member_kick:              20,
-      member_prune:             21,
-      member_ban_add:           22,
-      member_ban_remove:        23,
-      member_update:            24,
-      member_role_update:       25,
-      role_create:              30,
-      role_update:              31,
-      role_delete:              32,
-      invite_create:            40,
-      invite_update:            41,
-      invite_delete:            42,
-      webhook_create:           50,
-      webhook_update:           51,
-      webhook_delete:           52,
-      emoji_create:             60,
-      emoji_update:             61,
-      emoji_delete:             62,
-      message_delete:           72
-    }
-  end
-
-  @doc """
-  Returns an audit log object for the guild. Requires the 'VIEW_AUDIT_LOG' permission.
+  Returns an audit log object for the guild. Requires the `VIEW_AUDIT_LOG` permission.
 
   ## Options
 
@@ -54,21 +18,10 @@ defmodule Din.Resources.AuditLog do
   """
   @spec get_guild_audit_log(Din.snowflake, [
     user_id: Din.snowflake,
-    action_type: integer | event,
+    action_type: integer,
     before: Din.snowflake,
     limit: 1..100]) :: map | Error.t
   def get_guild_audit_log(guild_id, opts \\ []) do
-    opts = cond do
-      Keyword.has_key?(opts, :action_type) ->
-        cond do
-          is_atom(opts.action_type) ->
-            Keyword.update! opts, :action_type, &(event_codes()[&1])
-          true -> opts
-        end
-      true -> opts
-    end
-
-    query = URI.encode_query(opts)
-    Din.API.get "/guilds/#{guild_id}/audit-logs?#{query}"
+    Din.API.get "/guilds/#{guild_id}/audit-logs?#{URI.encode_query opts}"
   end
 end
