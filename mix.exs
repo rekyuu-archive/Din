f75d517cc8fca1d54663c89a241079e59b88a8ed
rekyuu <senkan.re.kyuu@gmail.com>
2019-08-23 19:46:56 -0600

    reverting

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Din.Mixfile do
   def project do
     [app: :din,
      version: "0.1.1",
-     elixir: "~> 1.9",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -32,7 +32,7 @@ defmodule Din.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:ex_doc, "~> 0.16", only: :dev, runtime: false},
-     {:websockex, github: "rekyuu/websockex"},
+     {:websockex, "~> 0.4.2"},
      {:poison, "~> 3.1"},
      {:httpoison, "~> 1.5"}]
   end
